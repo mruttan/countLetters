c3aa65a87957db8b6992822dd0e7745da7baa449
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-08 23:15:49 +0000

    Revised

--- count-letters.js
+++ count-letters.js
@@ -1,6 +1,6 @@
 function countLetters(string) {
   var count = {};
-  var compressed = string.split(' ').join('');
+  var compressed = string.split(' ').join('').toLowerCase();
   for(var i = 0; i < compressed.length; i++){
     if (count[compressed[i]]) count[compressed[i]]++;
     else count[compressed[i]] = 1;
