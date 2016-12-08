a1b25edb753163540f08d73f407ae2a7496ff443
Matt Ruttan <matthew.ruttan5@gmail.com>
2016-12-08 02:12:37 +0000

    Completed

---
+++ count-letters.js
@@ -0,0 +1,11 @@
+function countLetters(string) {
+  var count = {};
+  var compressed = string.split(' ').join('');
+  for(var i = 0; i < compressed.length; i++){
+    if (count[compressed[i]]) count[compressed[i]]++;
+    else count[compressed[i]] = 1;
+  }
+  return count;
+}  
+
+console.log(countLetters("lighthouse in the house"));
